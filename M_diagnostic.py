5dd493558d7164e4fa5a651a5cc7fb5f73bfc261
Joshua Zhou <jzhou103@uchicago.edu>
2024-12-05 16:29:10 -0600

    updating

--- M_diagnostic.py
+++ M_diagnostic.py
@@ -1,6 +1,18 @@
 import re, sys
 import numpy as np
 ifile = sys.argv[1]
+
+def calc_M_diagnostic(n_occ):
+    unocc = n_occ[n_occ<=0.25]
+    if (len(unocc) == 0):
+        unocc = [0]
+    docc = n_occ[n_occ>=1.75]
+    somo = n_occ[np.logical_and(n_occ < 1.75, n_occ > 0.25)]
+    if (len(docc) == 0):
+        docc = [2]
+    return 0.5 * (2 - min(docc) + sum(np.abs(somo-1)) + max(unocc))
+
+
 with open(ifile, 'r') as f:
     file_contents = f.read()
     matches = re.findall(r'Natural occ ([\[\d\. \n]*\])', file_contents, re.MULTILINE)
@@ -9,11 +21,5 @@ with open(ifile, 'r') as f:
     matches = list(L.values())
     
     for group in matches:
-        unocc = group[group<=0.25]
-        if (len(unocc) == 0):
-            unocc = [0]
-        docc = group[group>=1.75]
-        somo = group[np.logical_and(group < 1.75, group > 0.25)]
-        if (len(docc) == 0):
-            docc = [2]
-        print("M-Value:", 0.5 * (2 - min(docc) + sum(np.abs(somo-1)) + max(unocc))), sep='\t'
+        print(calc_M_diagnostic(group))        
+        
